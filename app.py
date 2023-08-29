3e0dbef2ae453e60591b08e5199af743a5b2e860
snoopyjosh <107114954+snoopyjosh@users.noreply.github.com>
2023-08-29 20:22:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,6 +13,29 @@ line_bot_api = LineBotApi(
 handler = WebhookHandler("549c652964c34ccf90318421ef371d04")
 
 
+def about_us_event(event):
+    emoji = [
+        {"index": 0, "productId": "5ac21184040ab15980c9b43a", "emojiId": "225"},
+        {"index": 17, "productId": "5ac21184040ab15980c9b43a", "emojiId": "225"},
+    ]
+
+    text_message = TextSendMessage(
+        text="""$ Master RenderP $
+Hello! 您好，歡迎您成為 Master RenderP 的好友！
+
+我是Master 支付小幫手 
+
+-這裡有商城，還可以購物喔~
+-直接點選下方【圖中】選單功能
+
+-期待您的光臨！""",
+        emojis=emoji,
+    )
+
+    sticker_message = StickerSendMessage(package_id="8522", sticker_id="16581271")
+    line_bot_api.reply_message(event.reply_token, [text_message, sticker_message])
+
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=["POST"])
 def callback():
